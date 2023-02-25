f8ff6db5e5f381ad18dd20b3cbb63f5aa6217120
ajitpatel28 <ajitpatel7389@gmail.com>
2023-02-25 12:07:39 +0530

    updating rust image sampler

--- Filter.rs
+++
@@ -1,7 +0,0 @@
-pub struct Filter <'a> {
-    /// The filter's filter function.
-    pub kernel: Box<Fn(f32) -> f32 + 'a>,
-
-    /// The window on which this filter operates.
-    pub support: f32
-}
--- sinc.rs
+++
@@ -1,9 +0,0 @@
-fn sinc(t: f32) -> f32 {
-    let a = t * f32::consts::PI;
-
-    if t == 0.0 {
-        1.0
-    } else {
-        a.sin() / a
-    }
-}
